9d4a2f3d9ee181e92d1037b71f04228c449bbf53
Arthur <arthurdevcontatoo@gmail.com>
2025-07-02 11:12:43 -0300

    matricular-alunos-e-cadastrar-disciplinas-finalizadas

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def exibir_menu_principal():
         print("4 - Lançar notas e frequência de alunos")
         print("5 - Listar desempenho do aluno")
         print("6 - Gerar boletim")
-        print("7 - Sair\n")
+        print("7 - Listar alunos")
+        print("8 - Sair\n")
 
         selecionar_opcao = input("\nInforme o número referente aopção desejada: ")
 
@@ -21,7 +22,10 @@ def exibir_menu_principal():
         if selecionar_opcao == 1:
             cadastrar_aluno()
         
-        elif selecionar_opcao == ...:
+        elif selecionar_opcao == 7:
+            listar_alunos()
+            
+        elif selecionar_opcao == 8:
             menu_principal = False
             print("\nMenu finalizado com sucesso!")
 
@@ -38,6 +42,17 @@ def cadastrar_aluno():
         print("\nAluno cadastrado com sucesso!")
 
 
+def listar_alunos():
+    if alunos:
+        print("\n=== LISTA DE ALUNOS ===\n")
+
+        i = 1
+        for code, aluno in alunos.items():
+            print(f"{i} - {aluno['Nome do aluno']}")
+            i += 1
+    else:
+        print("\nLista de alunos vazia!")
+    
 
 
 exibir_menu_principal()
