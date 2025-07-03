fe7323375a62058535c0605ff5f39209e485438d
Arthur <arthurdevcontatoo@gmail.com>
2025-07-03 09:40:37 -0300

    funcoes

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 alunos = {}
-lista_disciplinas = []
+DISCIPLINAS_PERMITIDAS = ['BIOLOGIA', 'FISICA', 'QUIMICA', 'MATEMATICA']
 
 def exibir_menu_principal():
     menu_principal = True
     while menu_principal:
         print("\n=== SISTEMA ESCOLAR ===\n")
         print("1 - Cadastrar alunos")
-        print("2 - Cadastrar disciplinas")
-        print("3 - Matricular alunos")
-        print("4 - Lançar notas e frequência de alunos")
-        print("5 - Listar desempenho do aluno")
-        print("6 - Gerar boletim")
-        print("7 - Listar alunos")
-        print("8 - Sair\n")
+        print("2 - Matricular alunos em disciplinas")
+        print("3 - Lançar notas e frequência de alunos")
+        print("4 - Listar desempenho do aluno")
+        print("5 - Gerar boletim")
+        print("6 - Listar alunos")
+        print("7 - Sair\n")
 
         selecionar_opcao = input("\nInforme o número referente aopção desejada: ")
 
@@ -23,17 +22,17 @@ def exibir_menu_principal():
 
         if selecionar_opcao == 1:
             cadastrar_aluno()
-        
+
         elif selecionar_opcao == 2:
-            cadastrar_disciplinas()
+            matricular_alunos_disciplinas()
 
         elif selecionar_opcao == 3:
-            matricular_alunos()
+            notas_frequencias()
 
-        elif selecionar_opcao == 7:
+        elif selecionar_opcao == 6:
             listar_alunos()
 
-        elif selecionar_opcao == 8:
+        elif selecionar_opcao == 7:
             menu_principal = False
             print("\nMenu finalizado com sucesso!")
 
@@ -50,99 +49,82 @@ def cadastrar_aluno():
         print("\nAluno cadastrado com sucesso!")
 
 
-def cadastrar_disciplinas():
-    nome_disciplina = input("\nInforme o nome da disciplina que deseja cadastrar: ").upper().strip()
-    lista_disciplinas.append(nome_disciplina)
+def matricular_alunos_disciplinas():
+    if alunos:
+        
+        i = 1
+        print("\n=== LISTA DE ALUNOS ===\n")
 
-    print("\nDisciplina cadastrada com sucesso!")
+        for code, nome in alunos.items():
+            print(f"{i} - Código: {code} | Nome do aluno: {nome['Nome do aluno']}")
+            i += 1
 
+        selecionar_aluno = input("\nInforme o código do aluno que deseja matricular em uma disciplina: ").strip().upper()
+        nome_disciplina = input("\nDigite o nome da disciplina que o aluno será matriculado: ").strip().upper()
 
-def matricular_alunos():
-    if lista_disciplinas:
+        if nome_disciplina in DISCIPLINAS_PERMITIDAS:
+            if nome_disciplina not in alunos[selecionar_aluno]:
+                alunos[selecionar_aluno]['Nome do aluno']['Disciplinas']= {'Disciplina': nome_disciplina}
 
-        if alunos:
-            print("\n=== LISTA DE ALUNOS ===\n")
-            i = 1
+                print("\nAluno matriculado com sucesso!")
 
-            for code, aluno in alunos.items():
-                print(f"{i} - Nome: {code['Nome do aluno']} | Código: {code}")
-                i += 1
-
-            selecionar_codigo_aluno = input("n\Informe o código do aluno que deseja realizar cadastrosde disciplinas: ").upper()
-
-            if selecionar_codigo_aluno in alunos:
-                i = 1
-
-                for lista in lista_disciplinas:
-                    print(f"{i} - {lista}")
-                    i += 1
-
-                selecionar_disciplina = input("\nInforme o número referente a disciplina que deseja cadastrar aluno: ").upper().strip()
-
-                try:
-                    selecionar_disciplina = int(selecionar_disciplina)
-                    selecionar_disciplina -= 1
-                except ValueError:
-                    print("\nSomente números!")
-
-                if 0 <= selecionar_disciplina and selecionar_disciplina <= len(lista_disciplinas):
-                    alunos[selecionar_codigo_aluno] = {'Disciplina': lista_disciplinas}
-                    print("\nAluno matriculado com sucesso!")
-
-                else:
-                    print("\Disciplina inválida!")
             else:
-                print("\nCódigo de aluno inválido!")
+                print("\nAluno já cadastrado na disciplina!")
         else:
-            print("\nLista de alunos vazia!")
+            print("\nDisciplina inválida!")
     else:
-        print("\nLista de disciplinas vazia!")
-    
+        print("\nSem alunos cadastrados!")
+
+
 def notas_frequencias():
     if alunos:
         print("\n=== LISTA DE ALUNOS ===\n")
 
         i = 1
         for code, nome in alunos.items():
-            print(f"{i} - Código: {code} | Nome: {code['Nome do aluno']}")
+            print(f"{i} - Código: {code} | Nome: {nome['Nome do aluno']}")
             i += 1
 
-        selecionar_codigo = input("\nInforme o código do aluno: ").upper().strip()
+        selecionar_aluno = input("\nInforme o código do aluno: ").upper().strip()
 
-        if selecionar_codigo in alunos:
-            if 'Disciplina' in alunos[selecionar_codigo]:
-                print("\n=== DISCIPLINAS DO ALUNO ===\n")
+        if selecionar_aluno in alunos:
 
-                for disciplina in disciplina[selecionar_codigo]['Diciplina']:
-                    print(f"Disciplina {i} - {disciplina['Nome da disciplina']}")
+                i = 1
+                print("\n=== LISTA DE DISCIPLINAS ===\n")
+
+                for code, disciplina in alunos.items():
+                    print(f"Disciplina {i} - {disciplina['Disciplinas']}")
                     i += 1
-                
-                selecionar_disciplina = input("\nInforme o nome da disciplina que serão lançadas as notas e frequências: ").strip().upper()
 
-                if selecionar_disciplina in alunos[selecionar_codigo]['Disciplina']:
-                    nota_1 = input("\nDigite a primeira nota do aluno: ")
-                    nota_2 = input("\Digite a segunda nota do aluno: ")
+                selecionar_disciplina = input("\nInforme o nome da disciplina para lançamento: ").strip().upper()
+
+                if selecionar_disciplina in DISCIPLINAS_PERMITIDAS:
+                    nota_1 = input("\nInforme a nota 01: ")
+                    nota_2 = input("Informe a nota 02: ")
+
+                    frequencia = input("\nInforme a frequência total do aluno [SEM CARACTERES ESPECIAIS]: ")
 
                     try:
                         nota_1 = float(nota_1)
                         nota_2 = float(nota_2)
-                    except:
+                        frequencia = float(frequencia)
+
+                    except ValueError:
                         print("\nSomente números!")
 
-                    media_final =(nota_1 + nota_2) / 2
+                    alunos[selecionar_aluno]['Disciplinas'][selecionar_disciplina]= {
+                        'Nota 1': nota_1,
+                        'Nota 2': nota_2,
+                        'Frequência': frequencia
+                    }
+
+                    print("\nNotas lançadas com sucesso!")
+
                 else:
-                    print("\nDisciplina não encontrada para este aluno!")
-            else:
-                print("\nO aluno não possui cadastro em nenhuma matéria!")
-            
+                    print("\nDisciplina inválida!")
+
         else:
-            print("\nAluno não encontrado!")
-    else:
-        print("\nLista de alunos vazia!")
-
-
-
-
+            print("\nEste aluno não foi cadastrado!")
 
 
 def listar_alunos():
